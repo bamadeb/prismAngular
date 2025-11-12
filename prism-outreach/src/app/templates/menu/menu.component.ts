79d5cd97dc84079cfb9b0dc8fdb7af9a12fe6388
padmaonly <pradhan.padma@gmail.com>
2025-11-12 16:43:02 +0530

    role wise menu

--- prism-outreach/src/app/templates/menu/menu.component.ts
+++ prism-outreach/src/app/templates/menu/menu.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent { }
+export class MenuComponent implements OnInit {
+  userRole: number | null = null;
+    constructor(
+      private auth: AuthService
+  ) {}
+  ngOnInit(): void {
+    const user = this.auth.getUser();
+    this.userRole = user.role_id;
+
+  }
+ }
